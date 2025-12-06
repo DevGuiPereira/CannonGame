2a64cb6facd4e75f7085bf24e5e3748471398709
Pedro Vargas <pedroangelovargas@outlook.com>
2025-12-06 18:49:35 -0300

    Adição das classes Cannon e Cannonball

--- app/src/main/java/com/example/cannongame/Cannon.java
+++ app/src/main/java/com/example/cannongame/Cannon.java
@@ -1,7 +1,75 @@
+// Representa o Canhão e dispara a Bola de Canhão
 package com.example.cannongame;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+
 public class Cannon {
+    private int baseRadius; // Raio da base do canhão
+    private int barrelLength;  // Comprimento do cano do canhão
+    private Point barrelEnd = new Point(); // Ponto final do cano do Canhão
+    private double barrelAngle; // ângulo do cano do Canhão
+    private Cannonball cannonball; // a Bola de Canhão do Canhão
+    private Paint paint = new Paint(); // Tinta usada para desenhar o canhão
+    private CannonView view;  // visualização contendo o Canhão
 
     public Cannon(CannonView view, int baseRadius, int barrelLength, int barrelWidth) {
+        this.view = view;
+        this.baseRadius = baseRadius;
+        this.barrelLength = barrelLength;
+        paint.setStrokeWidth(barrelWidth); // definir largura do cano
+        paint.setColor(Color.BLACK); // A cor do canhão é preta
+        align(Math.PI / 2); // Cano de canhão voltado para a direita
     }
-    public void draw(android.graphics.Canvas canvas) {}
+
+    public void align(double barrelAngle) { // alinha o cano do Canhão ao ângulo dado
+        this.barrelAngle = barrelAngle;
+
+        barrelEnd.x = (int) (barrelLength * Math.sin(barrelAngle));
+        barrelEnd.y = (int) (-barrelLength * Math.cos(barrelAngle)) + view.getScreenHeight() / 2;
+    }
+
+    // cria e dispara Cannonball na direção em que Cannon aponta
+    public void fireCannonball() {
+
+        // calcule o componente x da velocidade da bala de canhão
+        int velocityX = (int) (CannonView.CANNONBALL_SPEED_PERCENT *
+                view.getScreenWidth() * Math.sin(barrelAngle));
+
+        // calcule o componente y da velocidade da bala de canhão
+        int velocityY = (int) (CannonView.CANNONBALL_SPEED_PERCENT *
+                view.getScreenWidth() * -Math.cos(barrelAngle));
+
+        // calcule o raio da bala de canhão
+        int radius = (int) (view.getScreenHeight() * CannonView.CANNONBALL_RADIUS_PERCENT);
+
+        // construa Cannonball e posicione-o no Cannon
+        cannonball = new Cannonball(view, Color.BLACK,
+                CannonView.CANNON_SOUND_ID, -radius,
+                view.getScreenHeight() / 2 - radius, radius, velocityX, velocityY);
+
+        // reproduzir som de bola de canhão de fogo
+        cannonball.playSound();
+    }
+
+    // desenha o canhão na tela
+    public void draw(Canvas canvas) {
+
+        // desenhar cano de canhão
+        canvas.drawLine(0, view.getScreenHeight() / 2, barrelEnd.x, barrelEnd.y, paint);
+
+        // desenhar base do canhão
+        canvas.drawCircle(0, (int) view.getScreenHeight() / 2, (int) baseRadius, paint);
+    }
+
+    // retorna a Cannonball que este Cannon disparou
+    public Cannonball getCannonball() {
+        return cannonball;
+    }
+
+    // remove a Cannonball do jogo
+    public void removeCannonball() {
+        cannonball = null;
+    }
 }
--- app/src/main/java/com/example/cannongame/Cannonball.java
+++ app/src/main/java/com/example/cannongame/Cannonball.java
@@ -1,7 +1,60 @@
+// Representa a Bola de Canhão que o Canhão dispara
 package com.example.cannongame;
-public class Cannonball extends GameElement {
+import android.graphics.Canvas;
+import android.graphics.Rect;
 
-    public Cannonball(CannonView view, int color, int soundId, int x, int y, int radius, float velocityX, float velocityY) {
-        super(view, color, soundId, x, y, radius * 2, radius * 2, velocityY);
+public class Cannonball extends GameElement {
+    private float velocityX;
+    private boolean onScreen;
+
+    // construtor
+    public Cannonball(CannonView view, int color, int soundId, int x,
+                      int y, int radius, float velocityX, float velocityY) {
+        super(view, color, soundId, x, y, 2 * radius, 2 * radius, velocityY);
+        this.velocityX = velocityX;
+        onScreen = true;
+    }
+
+    // obtem o raio de Cannonball
+    private int getRadius() {
+        return (shape.right - shape.left) / 2;
+    }
+
+    // teste se Cannonball colide com o GameElement fornecido
+    public boolean collidesWith(GameElement element) {
+        return (Rect.intersects(shape, element.shape) && velocityX > 0);
+    }
+
+    // retorna verdadeiro se esta Cannonball estiver na tela
+    public boolean isOnScreen() {
+        return onScreen;
+    }
+
+    // inverte a velocidade horizontal do Cannonball
+    public void reverseVelocityX() {
+        velocityX *= -1;
+    }
+
+    // atualiza a posição do Cannonball
+    @Override
+    public void update(double interval) {
+        super.update(interval);  // atualiza a posição vertical do Cannonball
+
+        // atualizar posição horizontal
+        shape.offset((int) (velocityX * interval), 0);
+
+        // se Cannonball sair da tela
+        if (shape.top < 0 || shape.left < 0 ||
+                shape.bottom > view.getScreenHeight() ||
+                shape.right > view.getScreenWidth()) {
+            onScreen = false; // defina-o para ser removido
+        }
+    }
+
+    // desenha a Cannonball na tela fornecida
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawCircle(shape.left + getRadius(),
+                shape.top + getRadius(), getRadius(), paint);
     }
 }
